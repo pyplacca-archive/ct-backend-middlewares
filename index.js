1ea71f07540dd6a1a086fa7d9a593fb1d1543c6b
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-17 18:13:30 +0000

    complete phase one (more in commit description)
    1. Import server module and middleware.
    2. Setup model and storage bucket (array).
    3. Create request handlers.
    4. Apply middleware(s).
    5. Create routes to handle "get" and "post" requests.

---
+++ index.js
@@ -0,0 +1,49 @@
+const server = require('express')();
+const bodyParser = require('body-parser');
+
+
+// our makeshift database
+const banks = [];
+
+// our model
+class Bank {
+	constructor({name, location, branch}) {
+		this.name = name;
+		this.location = location;
+		this.branch = branch
+	}
+
+	addEntry = () => {
+		banks.push(this)
+		return this
+	}
+
+	static get entries () {
+		return banks
+	}
+}
+
+/*
+	Request handlers
+*/
+function createBankEntry (req, res) {
+	const entry = new Bank(req.body).addEntry();
+	res.status(201).json(entry);
+}
+
+function getBankEntries (req, res) {
+	res.json(Bank.entries);
+}
+
+
+// apply middleware(s)
+server.use(bodyParser.json());
+
+// our request listeners
+server.get('/banks', getBankEntries);
+server.post('/banks/add', createBankEntry)
+
+// start server
+server.listen(2021, () => {
+	console.log('Server is running...')
+});
