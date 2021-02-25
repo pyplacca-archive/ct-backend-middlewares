78121fe98d25b38cf86be4d1aff13a6b34211dbe
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-25 10:32:11 +0000

    refactoring: hide mongodb uri and, rewrite comments

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bankRouter = require('./routes/bankRoutes');
 
 const server = express();
-const { env } = process; // access our storage keys;
+const PORT = process.env.PORT || 2021;
 
-
-// apply all other middleware(s)
+// apply all general-purpose middleware(s)
 server.use(express.json());
-// then apply our "custom" router middleware
-server.use('/banks', bankRouter)
+server.use('/banks', bankRouter) // "custom" router middleware
 
-// connect our application to the database (mongodb)
-mongoose.connect(
-	`mongodb+srv://${env.DB_USER}:${env.DB_PASS}@cluster0.ombtf.mongodb.net/${env.DB_NAME}?retryWrites=true&w=majority`,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false
-	}
-)
-.then(res => {
-	console.log('Database connected!')
-	// start server if database connection is successful
-	server.listen(2021, () => {
-		console.log('Server is running at port 2021...')
+// connect our server to the mongodb database
+mongoose.connect(process.env.MONGDB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+})
+.then(() => {
+	// start our server after a successful connection to our database
+	server.listen(PORT, () => {
+		console.log(`Server is running at port ${PORT}...`)
 	});
 })
-.catch(err => {
+.catch(() => {
 	// handle database connection error
-	console.log(`Database connection err -> ${err?.message}`)
+	console.log('Unable to connect database. Please check your internet connection')
 })
