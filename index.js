ad1505fe3c42b53884f703963e2bef6bae282b4f
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-03 12:44:31 +0000

    Update index.js
    define account router as a middleware for the server

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bankRouter = require('./routes/bankRoutes');
+const routes = require('./routes');
 
 const server = express();
 const PORT = process.env.PORT || 2021;
 
 // apply all general-purpose middleware(s)
 server.use(express.json());
-server.use('/banks', bankRouter) // "custom" router middleware
+// "custom" router middleware
+server.use('/banks', routes.bankRouter)
+server.use('/accounts', routes.accountRouter)
 
 // connect our server to the mongodb database
-mongoose.connect(process.env.MONGDB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false
@@ -23,7 +25,8 @@ mongoose.connect(process.env.MONGDB_URI, {
 		console.log(`Server is running at port ${PORT}...`)
 	});
 })
-.catch(() => {
+.catch(err => {
+	console.log({MongooseErr: err})
 	// handle database connection error
 	console.log('Unable to connect database. Please check your internet connection')
 })
