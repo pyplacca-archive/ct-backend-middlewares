1db931a6bee052b6d8a1afa191605f8b501590ad
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-03 12:43:25 +0000

    Create accountController.js
    define specific controllers for creating and accessing customer accounts

---
+++ controllers/accountController.js
@@ -0,0 +1,27 @@
+// import api models
+const Account = require('../models/accountModel');
+
+
+function createAccountEntry (req, res) {
+	new Account(req.body).save()
+	.then(data => {
+		res.status(201).json({
+			message: 'Account created successfully',
+			data
+		});
+	})
+	.catch(err => res.status(500).json({message: err}))
+}
+
+function getAccountEntries (req, res) {
+	Account
+	.find()
+	.populate('bank', 'name address accountNumber branch')
+	.then(docs => res.json(docs))
+	.catch(err => res.status(500).json({message: err}))
+}
+
+module.exports = {
+	createAccountEntry,
+	getAccountEntries
+}
