e15c2dcfb99f31f1a41900e878fd5a1b6cfbd50a
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-10 11:52:25 +0000

    validate incoming post requests on the "/account/add" route

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,16 +1,22 @@
 // import api models
+const { validationResult } = require('express-validator');
 const Account = require('../models/accountModel');
 
 
 function createAccountEntry (req, res) {
-	new Account(req.body).save()
-	.then(data => {
-		res.status(201).json({
-			message: 'Account created successfully',
-			data
-		});
-	})
-	.catch(err => res.status(500).json({message: err}))
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		res.status(400).json(errors);
+	} else {
+		new Account(req.body).save()
+		.then(data => {
+			res.status(201).json({
+				message: 'Account created successfully',
+				data
+			});
+		})
+		.catch(err => res.status(500).json({message: err}))
+	}
 }
 
 function getAccountEntries (req, res) {
