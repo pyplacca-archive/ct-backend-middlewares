044f7b7cd1fb75c52977846939f0ee4db474ebb9
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-10 11:50:58 +0000

    integrate validation for account post requests

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
+const { body } = require('express-validator');
+
 const accountController = require('../controllers/accountController')
 
 
 // define routes for our bank api
 router.get('', accountController.getAccountEntries);
-router.post('/add', accountController.createAccountEntry);
+router.post('/add', [
+    ...[
+        ['accountName', 'Account name is required'],
+        ['accountNumber', 'Account number is required'],
+        ['accountType', 'Please specify the account type'],
+    ].map(arg => body(...arg).not().isEmpty())
+], accountController.createAccountEntry);
 
 module.exports = router;
