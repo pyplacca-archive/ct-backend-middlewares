d9d6e6f14db97ff0103fee0a26ce0ac671fa7025
pyPlacca <dhegreatprosperity10@gmail.com>
2021-03-17 14:16:07 +0000

    Update index.js
    1. apply userRouter as a middleware to app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const server = express();
 const PORT = process.env.PORT || 2021;
@@ -10,23 +9,27 @@ const PORT = process.env.PORT || 2021;
 // apply all general-purpose middleware(s)
 server.use(express.json());
 // "custom" router middleware
-server.use('/banks', routes.bankRouter)
-server.use('/accounts', routes.accountRouter)
+server.use("/banks", routes.bankRouter);
+server.use("/accounts", routes.accountRouter);
+server.use("/users", routes.userRouter);
 
 // connect our server to the mongodb database
-mongoose.connect(process.env.MONGODB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false
-})
-.then(() => {
-	// start our server after a successful connection to our database
-	server.listen(PORT, () => {
-		console.log(`Server is running at port ${PORT}...`)
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		// start our server after a successful connection to our database
+		server.listen(PORT, () => {
+			console.log(`Server is running at port ${PORT}...`);
+		});
+	})
+	.catch((err) => {
+		console.log({ MongooseErr: err });
+		// handle database connection error
+		console.log(
+			"Unable to connect database. Please check your internet connection"
+		);
 	});
-})
-.catch(err => {
-	console.log({MongooseErr: err})
-	// handle database connection error
-	console.log('Unable to connect database. Please check your internet connection')
-})
